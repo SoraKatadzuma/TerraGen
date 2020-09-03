be826d9b40c4416cc03c5f243d23b914a6cc029b
SoraKatadzuma <dungeonsandanime@gmail.com>
2020-09-03 12:05:19 -0500

    Single chunk generator waits for job

--- Assets/Scripts/ChunkGenerator.cs
+++ Assets/Scripts/ChunkGenerator.cs
@@ -1,4 +1,6 @@
 using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
 using UnityEngine;
 
 /// <summary>
@@ -36,6 +38,26 @@ public class ChunkGenerator : MonoBehaviour {
   private GameObject mChunk;
 
   /// <summary>
+  /// The generator that will generate the chunk.
+  /// </summary>
+  private ChunkGeneratorJob mGenerator;
+
+  /// <summary>
+  /// The handle to the generator job.
+  /// </summary>
+  private JobHandle mJobHandle;
+
+  /// <summary>
+  /// The chunk data we are trying to get.
+  /// </summary>
+  private NativeList<float3> mChunkData;
+
+  /// <summary>
+  /// Whether or not the job finished.
+  /// </summary>
+  private bool mJobFinished;
+
+  /// <summary>
   /// MAkes sure this object is initialized when it becomes usable in the engine.
   /// </summary>
   private void Start() {
@@ -50,17 +72,47 @@ public class ChunkGenerator : MonoBehaviour {
     mMeshFilter.sharedMesh       = mChunkMesh;
     mMeshRenderer                = mChunk.AddComponent<MeshRenderer>();
     mMeshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+    mChunkData                   = new NativeList<float3>(Allocator.TempJob);
 
-    // Generate mesh.
-    generate();
+    // Create generator.
+    mGenerator = new ChunkGeneratorJob {
+      chunkDataStorage = mChunkData,
+      chunkLocation    = new int3(0, 0, 0),
+      noiseSettings    = noiseSettings
+    };
+
+    // Schedule job.
+    mJobHandle   = mGenerator.Schedule();
+    mJobFinished = false;
 
     // Set this chunk position so we can compare it to the chunk we're trying to generate
     mChunk.transform.position = new Vector3(32.0f, -16.0f, -16.0f);
   }
 
-  const float RADIUS = 64.0f;
-  private bool inSphere(float x, float y, float z) {
-    return (x*x) + (y*y) + (z*z) < RADIUS;
+
+  private void Update() {
+    // Wait for job to complete.
+    if (!mJobHandle.IsCompleted || mJobFinished)
+      return;
+
+    // Finish job.
+    mJobHandle.Complete();
+    mJobFinished = true;
+
+    // Create mesh data.
+    var vertices  = new Vector3[mChunkData.Length];
+    var triangles = new int    [mChunkData.Length];
+    for (int index = 0; index < mChunkData.Length; index++) {
+      vertices [index] = mChunkData[index];
+      triangles[index] = index;
+    }
+
+    // Set vertices in chunk mesh.
+    mChunkMesh.vertices  = vertices;
+    mChunkMesh.triangles = triangles;
+    mChunkMesh.RecalculateNormals();
+    mChunkMesh.UploadMeshData(true);
+    mChunkData.Dispose();
   }
 
   /// <summary>
